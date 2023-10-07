40a4a496acec361c07bac139de6d33bab09ab0a1
Aryanmallik <87766085+Aryanmallik@users.noreply.github.com>
2023-10-07 09:35:33 +0530

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 export default function Header()
 {
-    return <header style={{backgroundColor:'pink',padding:10}}>
+    return <header style={{padding:10}}>
         <nav>
-            <ul>
+            <ul style={{fontSize:20, fontFamily:'cursive'}}>
                 <li><Link href="/">Home</Link></li>
                 <li><Link href="/about">About</Link></li>
             </ul>
