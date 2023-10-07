4007b9100d94df471909ba6f3fe727c7910d0f60
Aryanmallik <87766085+Aryanmallik@users.noreply.github.com>
2023-10-07 10:58:16 +0530

    Update todolist.jsx

--- components/todolist.jsx
+++ components/todolist.jsx
@@ -8,7 +8,7 @@ export default function ToDoList( ) {
     return (
           <div style={{color:'black',marginLeft:'30%', marginTop:'3%'}}>
                 <h1 class="marginLeft:auto"> 2-do list</h1> 
-         <input style={{width: '400px',padding: '10px',border: '1px solid #ccc',borderradius: '4px',boxsizing:' border-box'}}
+         <input style={{width: '30%',padding: '10px',border: '1px solid #ccc',borderradius: '4px',boxsizing:' border-box'}}
             value={todo}
             placeholder={"Enter task"}
             onInput={(evt) => setTodo( evt.target.value )}
