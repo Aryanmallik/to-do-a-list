f3733b77c11f002072e62a0efe25a855e3363f5c
Aryanmallik <87766085+Aryanmallik@users.noreply.github.com>
2023-10-08 22:56:51 +0530

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -7,6 +7,6 @@ export default function AboutPage()
       }}>
     <p>woof</p>
     <p>built by -</p>
-    <p><a href="https://www.instagram.com/aryanmallikk/">Aryan </a>and </a></p>
+    <p><a href="https://www.instagram.com/aryanmallikk/">Aryan </a>and</p>
     </div>
 }
