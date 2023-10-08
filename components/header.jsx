deacb068994d5d1fdb9731a6bb48af576de9bd70
Aryanmallik <87766085+Aryanmallik@users.noreply.github.com>
2023-10-08 22:41:06 +0530

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -1,12 +1,12 @@
-import Link from 'next/link';
-export default function Header()
-{
-    return <header style={{padding:10}}>
-        <nav>
-            <ul style={{fontSize:20, fontFamily:'cursive'}}>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/about">About</Link></li>
-            </ul>
-        </nav>
-    </header>;
-}
+    import Link from 'next/link';
+    export default function Header()
+    {
+        return <header style={{padding:10, marginLeft:'42%'}}>
+            <nav>
+                <ul style={{fontSize:20, fontFamily:'monospace'}}>
+                    <li><Link style={{color:'black'}} href="/">Home</Link></li>
+                    <li><Link style={{color:'black'}} href="/about">About</Link></li>
+                </ul>
+            </nav>
+        </header>;
+    }
